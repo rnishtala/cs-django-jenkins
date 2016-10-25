640734e40531eaead39da46af364182762f86c87
rnishtala <raj.nishtala@gmil.com>
2016-10-24 22:32:13 -0400

    first commit

---
+++ virtualenvs/blog/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/rnishtala/Desktop/PythonPractice/concrete/cs-django-jenkins/virtualenvs/blog/bin/python2
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
